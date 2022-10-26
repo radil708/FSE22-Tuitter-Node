5d92ab40fb8b8b5734fae68ac3ecc5219aeb4b59
radil <adil.r@northeastern.edu>
2022-10-26 12:27:07 -0400

    Changed default homepage

--- dist/server.js
+++ dist/server.js
@@ -2,7 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const app = express();
-app.get('/', (req, res) => res.send('Welcome WebDev!'));
+function defaultPage(req, res) {
+    res.send('Welcome to Ramzi\'s Fall2022 SoftEng Home Page');
+}
+app.get('/', defaultPage);
 const PORT = 3000;
 app.listen(process.env.PORT || PORT);
 //# sourceMappingURL=server.js.map
