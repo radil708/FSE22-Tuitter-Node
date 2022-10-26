11a4a426bde6584f555cf7afc2d902708792cf48
radil <adil.r@northeastern.edu>
2022-10-26 13:21:59 -0400

    - Connected application to remote database successfully!

--- dist/server.js
+++ dist/server.js
@@ -2,10 +2,27 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose');
+/*************    Connect To Remote MongoDB Database    *****************/
+/*IMPORTANT** Make sure environment variable set up before running
+i.e. for aws elastic beanstalk deployment: eb setenv DB_CLUSTER_USERNAME=<username>
+on ubuntu use cmd: export DB_CLUSTER_USERNAME=<username>
+ */
+const DB_USERNAME = process.env.DB_CLUSTER_USERNAME;
+const DB_PASSWORD = process.env.DB_CLUSTER_PASSWORD;
+const DB_PROTOCOL = "mongodb+srv";
+const ENDING_QUERY = "retryWrites=true&w=majority";
+// Get this from Connect Application section of MongoDB Atlas online
+const HOST = "f22-softeng-cluster0.cxnwsxd.mongodb.net";
+// create this db in MongoDB via connection with MongoDB Compass GUI
+const DB_NAME = "fsd";
+const connectionString = DB_PROTOCOL + "://" + DB_USERNAME + ":" + DB_PASSWORD + "@" + HOST +
+    "/" + DB_NAME + "?" + ENDING_QUERY;
+mongoose.connect(connectionString);
 function defaultPage(req, res) {
     res.send('Welcome to Ramzi\'s Fall2022 SoftEng Home Page');
 }
 app.get('/', defaultPage);
-const PORT = 3000;
+const PORT = 4000;
 app.listen(process.env.PORT || PORT);
 //# sourceMappingURL=server.js.map
--- server.ts
+++ server.ts
@@ -2,6 +2,32 @@ import * as express from 'express';
 import {Request, Response} from "express";
 
 const app = express();
+const mongoose = require('mongoose');
+
+
+/*************    Connect To Remote MongoDB Database    *****************/
+
+/*IMPORTANT** Make sure environment variable set up before running
+i.e. for aws elastic beanstalk deployment: eb setenv DB_CLUSTER_USERNAME=<username>
+on ubuntu use cmd: export DB_CLUSTER_USERNAME=<username>
+ */
+const DB_USERNAME = process.env.DB_CLUSTER_USERNAME;
+const DB_PASSWORD = process.env.DB_CLUSTER_PASSWORD;
+const DB_PROTOCOL = "mongodb+srv";
+const ENDING_QUERY = "retryWrites=true&w=majority";
+
+// Get this from Connect Application section of MongoDB Atlas online
+const HOST = "f22-softeng-cluster0.cxnwsxd.mongodb.net";
+
+// create this db in MongoDB via connection with MongoDB Compass GUI
+const DB_NAME = "fsd"
+
+const connectionString = DB_PROTOCOL + "://" + DB_USERNAME + ":" + DB_PASSWORD + "@" + HOST +
+    "/" + DB_NAME + "?" + ENDING_QUERY
+
+mongoose.connect(connectionString)
+
+
 
 function defaultPage (req: Request, res: Response) {
     res.send('Welcome to Ramzi\'s Fall2022 SoftEng Home Page')
@@ -9,5 +35,5 @@ function defaultPage (req: Request, res: Response) {
 
 app.get('/',defaultPage)
 
-const PORT = 3000;
+const PORT = 4000;
 app.listen(process.env.PORT || PORT);
