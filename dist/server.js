f41bbc8630dc713561c20e20bb4a9465f41db325
radil <adil.r@northeastern.edu>
2022-10-26 11:29:32 -0400

    changed tsconfig and package.json

--- dist/server.js
+++ dist/server.js
@@ -1,30 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 /**
  * @file Implements an Express Node HTTP server.
  */
-import * as express from 'express';
-import {Request, Response} from 'express';
-import mongoose from "mongoose";
-
-
+const express = require("express");
 const app = express();
-
 app.use(express.json()); // Allows server to parse data coming from clients
-
-function sayHello (req: Request, res: Response) {
-    res.send('Hi from FSD!')
+function sayHello(req, res) {
+    res.send('Hi from FSD!');
 }
-
-function defaultPage (req: Request, res: Response) {
+function defaultPage(req, res) {
     res.send('Welcome to Ramzi\'s FS22 SoftEng AWS server');
 }
-
 app.get('/', defaultPage);
 app.get('/hello', sayHello);
-
 /**
  * Start a server listening at port 4000 locally
  * but use environment variable PORT on Heroku/AWS if available.
  */
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
-
+//# sourceMappingURL=server.js.map
