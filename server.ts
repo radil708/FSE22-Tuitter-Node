3974973a6542c194268e2e9dd439bf135b9463a1
radil <adil.r@northeastern.edu>
2022-10-26 12:23:07 -0400

    Functioning AWS deployment

--- server.ts
+++ server.ts
@@ -3,8 +3,11 @@ import {Request, Response} from "express";
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) =>
-    res.send('Welcome WebDev!'));
+function defaultPage (req: Request, res: Response) {
+    res.send('Welcome to Ramzi\'s Fall2022 SoftEng Home Page')
+}
+
+app.get('/',defaultPage)
 
 const PORT = 3000;
 app.listen(process.env.PORT || PORT);
