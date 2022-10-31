2cfc707006e025c0cd61096b0480a745bf91a6e2
Ramzi Reilly Adil <83944969+radil708@users.noreply.github.com>
2022-10-31 17:43:10 -0400

    Deleting dist directory

--- dist/Likes/jsonToUser.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const User_1 = require("../Users/User");
-function jsonToUser(userFromDb) {
-    return new User_1.default(userFromDb._id.toString() || '', userFromDb['username'] || '', userFromDb['firstName'] || '', userFromDb['lastName'] || '', userFromDb['password'] || '', userFromDb['email'] || '');
-}
-exports.default = jsonToUser;
-//# sourceMappingURL=jsonToUser.js.map
